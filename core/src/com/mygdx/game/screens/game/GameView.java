30c06d2b8a587fe3b42c298f00baf9e66489fead
Anders Solberg <ssredna@gmail.com>
2021-03-25 13:26:38 +0100

    PA-14: initialize ecs engine; using rendersystem and simple player entity

--- core/src/com/mygdx/game/screens/game/GameView.java
+++ core/src/com/mygdx/game/screens/game/GameView.java
@@ -1,10 +1,17 @@
 package com.mygdx.game.screens.game;
 
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.ecs.components.PositionComponent;
+import com.mygdx.game.ecs.components.SpriteComponent;
+import com.mygdx.game.ecs.systems.RenderSystem;
 import com.mygdx.game.screens.navigation.NavigatorController;
 
 public class GameView implements Screen {
@@ -12,10 +19,36 @@ public class GameView implements Screen {
     private Stage stage;
     private NavigatorController navigatorController;
 
+    private PooledEngine engine;
+
     public GameView(NavigatorController navigatorController) {
         this.navigatorController = navigatorController;
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
+        initializeEngine();
+    }
+
+    private void initializeEngine() {
+        this.engine = new PooledEngine();
+
+        // Todo: extract player creation to its own factory
+        Entity player = engine.createEntity();
+
+        PositionComponent position = engine.createComponent(PositionComponent.class);
+        SpriteComponent sprite = engine.createComponent(SpriteComponent.class);
+
+        Texture playerSprite = new Texture("sprites/player.png");
+        sprite.textureRegion = new TextureRegion(playerSprite);
+
+        position.position.x = 200;
+        position.position.y = 200;
+
+        player.add(position);
+        player.add(sprite);
+
+        engine.addEntity(player);
+
+        engine.addSystem(new RenderSystem());
     }
 
     @Override
@@ -30,6 +63,7 @@ public class GameView implements Screen {
 
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
+        engine.update(delta);
     }
 
     @Override
