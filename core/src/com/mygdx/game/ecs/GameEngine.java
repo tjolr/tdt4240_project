3c7aab3ff1497f1f251820437c1822fde71fa5b5
Anders Solberg <ssredna@gmail.com>
2021-03-26 11:52:39 +0100

    PA-27: add factory for player, and extract game engine to own class

---
+++ core/src/com/mygdx/game/ecs/GameEngine.java
@@ -0,0 +1,32 @@
+package com.mygdx.game.ecs;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.PooledEngine;
+import com.mygdx.game.ecs.entities.EntityFactory;
+import com.mygdx.game.ecs.systems.RenderSystem;
+
+public class GameEngine extends PooledEngine {
+    // GameEngine is a Singleton class
+    private static GameEngine gameEngineInstance = null;
+
+    private final EntityFactory entityFactory;
+
+    private GameEngine() {
+        entityFactory = EntityFactory.getInstance();
+    }
+
+    public static GameEngine getInstance() {
+        if (gameEngineInstance == null)
+            gameEngineInstance = new GameEngine();
+
+        return gameEngineInstance;
+    }
+
+    public void initializeEngine() {
+        Entity player = entityFactory.createPlayer(200, 200);
+
+        gameEngineInstance.addEntity(player);
+
+        gameEngineInstance.addSystem(new RenderSystem());
+    }
+}
