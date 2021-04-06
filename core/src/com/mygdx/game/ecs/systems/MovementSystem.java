ea58be546f98af20f7180b6832b158a38cd464f4
Vidar <vidarmic@gmail.com>
2021-04-06 13:11:52 +0200

    PA-15: implement basic movement system

--- core/src/com/mygdx/game/ecs/systems/MovementSystem.java
+++ core/src/com/mygdx/game/ecs/systems/MovementSystem.java
@@ -7,17 +7,24 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.mygdx.game.ecs.components.PositionComponent;
 import com.mygdx.game.ecs.components.VelocityComponent;
 
+// Skal kun være for å faktisk oppdatere posisjon, skal også bry seg om collision etter hvert.
 public class MovementSystem extends IteratingSystem {
-    private ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
-    private ComponentMapper<VelocityComponent> vm = ComponentMapper.getFor(VelocityComponent.class);
+    private ComponentMapper<PositionComponent> positionMapper;
+    private ComponentMapper<VelocityComponent> velocityMapper;
 
     public MovementSystem() {
         super(Family.all(PositionComponent.class, VelocityComponent.class).get());
+
+        positionMapper = ComponentMapper.getFor(PositionComponent.class);
+        velocityMapper = ComponentMapper.getFor(VelocityComponent.class);
     }
 
     public void processEntity(Entity entity, float deltaTime) {
-        PositionComponent position = pm.get(entity);
-        VelocityComponent velocity = vm.get(entity);
+        PositionComponent position = positionMapper.get(entity);
+        VelocityComponent velocity = velocityMapper.get(entity);
+
+        // The number should be between 300-600
+        position.position.add(velocity.velocity.scl(deltaTime * 400));
     }
 }
 
